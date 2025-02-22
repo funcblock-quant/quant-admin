ca18c27d8e769953778df6e997a3c6085a5abaf8
Amos <leeyiliuuu@163.com>
2025-02-22 14:00:25 +0700

    新需求更新

--- app/business/models/bus_price_trigger_strategy_instance.go
+++ app/business/models/bus_price_trigger_strategy_instance.go
@@ -9,15 +9,22 @@ import (
 type BusPriceTriggerStrategyInstance struct {
 	models.Model
 
-	OpenPrice      string    `json:"openPrice" gorm:"type:varchar(32);comment:开仓价格"`
-	ClosePrice     string    `json:"closePrice" gorm:"type:varchar(32);comment:平仓价格"`
-	Amount         string    `json:"amount" gorm:"type:varchar(32:);comment:开仓数量"`
-	Side           string    `json:"side" gorm:"type:varchar(16);comment:买卖方向"`
-	Symbol         string    `json:"symbol" gorm:"type:varchar(64);comment:交易币种"`
-	CloseTime      time.Time `json:"closeTime" gorm:"type:timestamp;comment:停止时间"`
-	ApiConfig      int       `json:"apiConfig" gorm:"type:timestamp;comment:api配置id"`
-	Status         string    `json:"status" gorm:"type:varchar(16);comment:状态，created, started, stopped, closed"`
-	ExchangeUserId string    `json:"exchangeUserId" gorm:"type:varchar(255);comment:交易所userId"`
+	OpenPrice         string    `json:"openPrice" gorm:"type:varchar(32);comment:开仓价格"`
+	ClosePrice        string    `json:"closePrice" gorm:"type:varchar(32);comment:平仓价格"`
+	Amount            string    `json:"amount" gorm:"type:varchar(32:);comment:开仓数量"`
+	Side              string    `json:"side" gorm:"type:varchar(16);comment:买卖方向"`
+	Symbol            string    `json:"symbol" gorm:"type:varchar(64);comment:交易币种"`
+	CloseTime         time.Time `json:"closeTime" gorm:"type:timestamp;comment:停止时间"`
+	ApiConfig         int       `json:"apiConfig" gorm:"type:timestamp;comment:api配置id"`
+	Status            string    `json:"status" gorm:"type:varchar(16);comment:状态，created, started, stopped, closed"`
+	ExchangeUserId    string    `json:"exchangeUserId" gorm:"type:varchar(255);comment:交易所userId"`
+	ExecuteNum        int       `json:"executeNum" gorm:"type:int;comment:执行次数"`
+	ProfitTargetType  string    `json:"profitTargetType" gorm:"type:varchar(32);comment:止盈类型"`
+	ProfitTargetPrice string    `json:"profitTargetPrice" gorm:"type:decimal(32,16);comment:限价止盈价格"`
+	LossTargetPrice   string    `json:"lossTargetPrice" gorm:"type:decimal(32,16);comment:限价止盈价格"`
+	CallbackRatio     *float64  `json:"callbackRatio" gorm:"type:float;comment:浮动止盈回调比例"`
+	CutoffRatio       *float64  `json:"cutoffRatio" gorm:"type:float;comment:浮动止盈止盈比例"`
+	MinProfit         *float64  `json:"minProfit" gorm:"type:float;comment:浮动止盈最低利润"`
 	models.ModelTime
 	models.ControlBy
 }
