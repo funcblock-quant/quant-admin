d876f552526a7dcb8c23ada177a51db87d04fbfb
Amos <leeyiliuuu@163.com>
2025-03-06 17:24:51 +0700

    update

--- app/jobs/simple_jobs.go
+++ app/jobs/simple_jobs.go
@@ -20,9 +20,7 @@ func InitSimpleJob() {
 	fmt.Printf("Init simple job \n")
 	orm := sdk.Runtime.GetDbByKey("*")               // 复用数据库连接
 	log := logger.NewHelper(sdk.Runtime.GetLogger()) // 复用日志对象
-	c.AddFunc("@every 5s", safeWrapper("", func() {
-		jobLock.Lock()
-		defer jobLock.Unlock()
+	c.AddFunc("@every 5s", func() {
 
 		// 每5s一次，获取最新的实时价差
 		fmt.Println("GetLatestSpreadData Job running")
@@ -33,11 +31,9 @@ func InitSimpleJob() {
 		if err != nil {
 			log.Errorf("GetLatestSpreadData failed, err:%v\n", err)
 		}
-	}))
+	})
 
-	c.AddFunc("@every 5s", safeWrapper("", func() {
-		jobLock.Lock()
-		defer jobLock.Unlock()
+	c.AddFunc("@every 5s", func() {
 		// 每5s一次，获查看链上链下套利实例水位调节情况
 		fmt.Println("Monitor WaterLevel Job running")
 		s := service.BusDexCexTriangularObserver{}
@@ -47,11 +43,9 @@ func InitSimpleJob() {
 		if err != nil {
 			log.Errorf("Monitor WaterLevel Job run failed, err:%v\n", err)
 		}
-	}))
+	})
 
-	c.AddFunc("@every 5s", safeWrapper("", func() {
-		jobLock.Lock()
-		defer jobLock.Unlock()
+	c.AddFunc("@every 5s", func() {
 		// 每5s一次，获查看交易中的实例的水位健康状态
 		fmt.Println("MonitorWaterLevelToStopTrader Job running")
 		s := service.BusDexCexTriangularObserver{}
@@ -61,11 +55,9 @@ func InitSimpleJob() {
 		if err != nil {
 			log.Errorf("MonitorWaterLevelToStopTrader Job run failed, err:%v\n", err)
 		}
-	}))
+	})
 
-	c.AddFunc("@every 1m", safeWrapper("", func() {
-		jobLock.Lock()
-		defer jobLock.Unlock()
+	c.AddFunc("@every 1m", func() {
 		// 每5s一次，启动全局水位调整功能
 		fmt.Println("StartGlobalWaterLevelConfig Job running")
 		s := service.BusDexCexTriangularObserver{}
@@ -75,11 +67,9 @@ func InitSimpleJob() {
 		if err != nil {
 			log.Errorf("StartGlobalWaterLevelConfig Job run failed, err:%v\n", err)
 		}
-	}))
+	})
 
-	c.AddFunc("@every 1m", safeWrapper("", func() {
-		jobLock.Lock()
-		defer jobLock.Unlock()
+	c.AddFunc("@every 1m", func() {
 		// 每1m一次，对于交易进行简单封控和告警
 		fmt.Println("Start Scan dex cex trade records Job running")
 		s := service.StrategyDexCexTriangularArbitrageTrades{}
@@ -89,11 +79,9 @@ func InitSimpleJob() {
 		if err != nil {
 			log.Errorf("StartGlobalWaterLevelConfig Job run failed, err:%v\n", err)
 		}
-	}))
+	})
 
-	c.AddFunc("@every 5s", safeWrapper("", func() {
-		jobLock.Lock()
-		defer jobLock.Unlock()
+	c.AddFunc("@every 5s", func() {
 		// 每5s一次，查看期权下单策略的执行次数是否到期
 		fmt.Println("Monitor Price Trigger ExecuteNum Job running")
 		s := service.BusPriceTriggerStrategyInstance{}
@@ -104,11 +92,9 @@ func InitSimpleJob() {
 			log.Errorf("Monitor Price Trigger ExecuteNum Job run failed, err:%v\n", err)
 		}
 
-	}))
+	})
 
-	c.AddFunc("@every 5s", safeWrapper("", func() {
-		jobLock.Lock()
-		defer jobLock.Unlock()
+	c.AddFunc("@every 5s", func() {
 
 		// 每5s一次风控检查
 		fmt.Println("CheckRisk Control Job running")
@@ -120,11 +106,9 @@ func InitSimpleJob() {
 			log.Errorf("CheckRiskControl Job run failed, err:%v\n", err)
 		}
 
-	}))
+	})
 
-	c.AddFunc("@every 5s", safeWrapper("", func() {
-		jobLock.Lock()
-		defer jobLock.Unlock()
+	c.AddFunc("@every 5s", func() {
 
 		// 每5s一次检查存在未处理的暂停交易的风控事件，并检查对应的交易实例是否暂停，作为补偿
 		fmt.Println("Check Exist Risk Event Job running")
@@ -136,11 +120,9 @@ func InitSimpleJob() {
 			log.Errorf("Check Exist Risk Event Job run failed, err:%v\n", err)
 		}
 
-	}))
+	})
 
-	c.AddFunc("@every 5s", safeWrapper("", func() {
-		jobLock.Lock()
-		defer jobLock.Unlock()
+	c.AddFunc("@every 5s", func() {
 
 		// 每5s一次检查被blocking的交易实例，如果所有风控都被恢复，也需要重新启动交易
 		fmt.Println("Check Blocking Instance Job running")
@@ -152,7 +134,7 @@ func InitSimpleJob() {
 			log.Errorf("Check Exist Risk Event Job run failed, err:%v\n", err)
 		}
 
-	}))
+	})
 
 	c.Start()
 	select {}
