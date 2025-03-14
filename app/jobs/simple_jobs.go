b111c9ea3b9f57857868db5146cf75b3ea783d20
Amos <leeyiliuuu@163.com>
2025-03-14 08:32:54 +0700

    update

--- app/jobs/simple_jobs.go
+++ app/jobs/simple_jobs.go
@@ -135,8 +135,7 @@ func InitSimpleJob() {
 
 	})
 
-	// c.AddFunc("0 0 * * *", func() {
-	c.AddFunc("@every 10m", func() {
+	c.AddFunc("0 0 * * *", func() {
 
 		// 每天凌晨12点，快照前一天的收益等数据
 		fmt.Println("Check Blocking Instance Job running")
