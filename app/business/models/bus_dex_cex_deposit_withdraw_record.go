118d4c99c6c4d381f81d01cb30bb98d8e2fb8cba
Amos <leeyiliuuu@163.com>
2025-04-11 18:54:10 +0700

    update

--- app/business/models/bus_dex_cex_deposit_withdraw_record.go
+++ app/business/models/bus_dex_cex_deposit_withdraw_record.go
@@ -9,20 +9,21 @@ import (
 type BusDexCexDepositWithdrawRecord struct {
 	models.Model
 
-	OrderType   string    `json:"orderType" gorm:"type:tinyint;comment:订单类型"`
-	FromAddress string    `json:"fromAddress" gorm:"type:varchar(255);comment:from地址"`
-	ToAddress   string    `json:"toAddress" gorm:"type:varchar(255);comment:to地址"`
-	CexUid      string    `json:"cexUid" gorm:"type:varchar(64);comment:cex账户uid"`
-	CexOrderId  string    `json:"cexOrderId" gorm:"type:varchar(255);comment:交易所充提业务id"`
-	TxHash      string    `json:"txHash" gorm:"type:varchar(255);comment:链上交易hash"`
-	Symbol      string    `json:"symbol" gorm:"type:varchar(64);comment:充提币种"`
-	Amount      string    `json:"amount" gorm:"type:decimal(32,16);comment:充提数量"`
-	Fee         string    `json:"fee" gorm:"type:decimal(32,16);comment:手续费"`
-	FeeAsset    string    `json:"feeAsset" gorm:"type:varchar(64);comment:手续费币种"`
-	StartTime   time.Time `json:"startTime" gorm:"type:timestamp;comment:发起时间"`
-	FinishTime  time.Time `json:"finishTime" gorm:"type:timestamp;comment:完成时间"`
-	Status      string    `json:"status" gorm:"type:tinyint;comment:状态"`
-	Remark      string    `json:"remark" gorm:"type:text;comment:备注"`
+	OrderType    string    `json:"orderType" gorm:"type:tinyint;comment:订单类型"`
+	FromAddress  string    `json:"fromAddress" gorm:"type:varchar(255);comment:from地址"`
+	ToAddress    string    `json:"toAddress" gorm:"type:varchar(255);comment:to地址"`
+	CexUid       string    `json:"cexUid" gorm:"type:varchar(64);comment:cex账户uid"`
+	CexOrderId   string    `json:"cexOrderId" gorm:"type:varchar(255);comment:交易所充提业务id"`
+	TxHash       string    `json:"txHash" gorm:"type:varchar(255);comment:链上交易hash"`
+	Symbol       string    `json:"symbol" gorm:"type:varchar(64);comment:充提币种"`
+	Amount       string    `json:"amount" gorm:"type:decimal(32,16);comment:充提数量"`
+	Fee          string    `json:"fee" gorm:"type:decimal(32,16);comment:手续费"`
+	FeeAsset     string    `json:"feeAsset" gorm:"type:varchar(64);comment:手续费币种"`
+	StartTime    time.Time `json:"startTime" gorm:"type:timestamp;comment:发起时间"`
+	FinishTime   time.Time `json:"finishTime" gorm:"type:timestamp;comment:完成时间"`
+	Status       string    `json:"status" gorm:"type:tinyint;comment:状态"`
+	Remark       string    `json:"remark" gorm:"type:text;comment:备注"`
+	ExchangeType string    `json:"exchangeType" gorm:"type:varchar(64);comment:交易所"`
 	models.ModelTime
 	models.ControlBy
 }
